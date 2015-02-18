0cedc3ec8f9ab9008698f7c1b4c56b05778c46df
946336 <946336@users.noreply.github.com>
2015-02-18 00:22:59 -0500

    Changed a bit of test

--- mainTest.cpp
+++ mainTest.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 /*
 apparently needed for my local copy of clang++ to work properly.
-funny, b/c sometimes it straigh up can't find iostream. :\
+funny, b/c sometimes it straight up can't find iostream. :\
 */
 void *__gxx_personality_v0;
 
@@ -12,7 +12,7 @@ int main(){
 	Queue<double> was;
 
 	for(int i = 0; i < 20; i++){
-		was.enqueue(double(7)/3);
+		was.enqueue(double(i)/3);
 	}
 
 	for(int i = 0; i < 20; i++){
